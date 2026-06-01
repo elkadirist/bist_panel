3bd42c8724d0bc726e4f0253dcca290dd086c395
elkadirist <elkadirist@hotmail.com>
2026-06-02 00:54:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("BIST ROBOT ÇALIŞIYOR")
 st.success("App başarılı şekilde açıldı")
+
+st.set_page_config(
+    page_title="BIST Robot",
+    page_icon="📊",
+    layout="wide"
+)
